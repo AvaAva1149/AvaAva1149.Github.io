4ea253e3a16e747a97416c2551edb25467cbe264
Ava <zahra.arjmandd@gmail.com>
2021-01-30 19:06:36 -0500

    add content
    make content

--- script.js
+++ script.js
@@ -1,6 +1,17 @@
 function start(){
-    $("body").css("background-color", "azure")
+    $("body").css("background-color", "peru")
+    $("#menu a:first").click();
 }
-
-
 $(window).on("load", start)
+
+
+function loadpage(e){
+    e.preventDefault();
+    //alert("ooo");
+    $(this).addclass("active");
+    $("#menu a.active").removeclass("active");
+    
+    var href = $(this).attr("href");    
+    $("#content").load(href);
+}
+$(document).on("click","#menu a", loadpage)
